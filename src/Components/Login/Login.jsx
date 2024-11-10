60ec8ac3ebf4e3024594de2c5400347d5c461aac
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-11-10 15:23:58 +0600

    add password eye icon close and open and add terms and conditon.

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,22 +1,32 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import auth from "../Firebase/Firebase.init";
+import { IoEyeSharp } from "react-icons/io5";
+import { IoMdEyeOff } from "react-icons/io";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSignUp = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password);
+    const terms = e.target.terms.checked;
+    console.log(email, password, terms);
 
     setErrorMessage("");
     setSuccess(false);
 
     if (password < 6) {
       setErrorMessage("Password must be more than 6 char.");
+      return;
+    }
+
+    if (!terms) {
+      setErrorMessage("Please Accept Terms & Condition");
       return;
     }
 
@@ -57,17 +67,27 @@ const Login = () => {
             required
           />
         </div>
-        <div className="form-control">
+        <div className="form-control relative">
           <label className="label">
             <span className="label-text text-purple-600">Password</span>
           </label>
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="password"
             className="input input-bordered"
             required
           />
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className="text-2xl absolute right-4 top-12"
+          >
+            {showPassword ? (
+              <IoMdEyeOff></IoMdEyeOff>
+            ) : (
+              <IoEyeSharp></IoEyeSharp>
+            )}
+          </button>
           <label className="label">
             <a
               href="#"
@@ -77,20 +97,31 @@ const Login = () => {
             </a>
           </label>
         </div>
+
+        <div className="form-control ">
+          <label className="cursor-pointer label justify-start">
+            <input
+              type="checkbox"
+              name="terms"
+              className="checkbox checkbox-success"
+            />
+            <span className="label-text ml-2">Accept Terms and Condition</span>
+          </label>
+        </div>
+
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
         </div>
       </form>
-
       {errorMessage && (
         <p className="text-xl font-bold text-center text-red-600">
           {errorMessage}
         </p>
       )}
-
       {success && (
         <p className="text-xl text-green-600 ml-28">
           Sing Up Successfully. Hurray..!!!
+          {/* {toast.success("Sign Up successfully")} */}
         </p>
       )}
     </div>
